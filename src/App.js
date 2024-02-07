d800792f63c6e43d9fed2f7cf2d93c909ae664b8
livi <scholtz.ice@gmail.com>
2024-02-07 16:19:13 +0200

    Removed puppeteer package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-import { useState } from "react";
-
-const puppeteer = require("puppeteer");
-
 const App = () => {
-    const [productImgUrls, setProductImgUrls] = useState([]);
-
-    const scrapeProduct = async (url) => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        const element = await page.waitForSelector("div > img");
-
-        const src = await element.getProperty("src");
-        const srcTxt = await src.jsonValue();
-
-        console.log({ srcTxt });
-    };
-
-    scrapeProduct("https://superbalist.com/wishlist/1890d14248803");
-
     return (
         <div>
             <header>
